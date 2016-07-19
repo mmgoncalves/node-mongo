f453d091dc33ad2d82aca1612813b7af5eae454a
Mateus Marques <marques.m05@live.com>
2016-07-19 00:22:08 -0300

    first commit

---
+++ routes/Person.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var Person = require('./../models/Person');
+
+// find all
+router.get('/', function(req, res){
+    Person.find({}, function(err, people){
+        if(err){
+            return;
+        }
+
+        res.send(people);
+    });
+});
+
+//findOne
+router.get('/:id', function(req, res){
+    Person.findOne({
+        _id: req.params.id
+    }, function(err, person){
+        if(err)
+            return;
+
+        res.send(person);
+    })
+});
+
+module.exports = router;
