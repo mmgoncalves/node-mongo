955e676a3850dfff9e6ada43ee3260bd72a0b38e
Mateus Marques <marques.m05@gmail.com>
2016-07-20 10:24:07 -0300

    fazendo update

--- routes/Person.js
+++ routes/Person.js
@@ -25,4 +25,102 @@ router.get('/:id', function(req, res){
     })
 });
 
+router.post('/', function(req, res){
+    var arr = [{
+        name: {
+            firstname: "Alvim 1",
+            lastname: "Brand 1"
+        },
+        age: 24
+    }, {
+        name: {
+            firstname: "Alvim 2",
+            lastname: "Brand 2"
+        },
+        age: 25
+    }, {
+        name: {
+            firstname: "Alvim 3",
+            lastname: "Brand 3"
+        },
+        age: 26
+    }];
+    
+    Person.insertMany(arr, function (err, person) {
+        if(err){
+            return;
+        }
+
+        res.send(person);
+    });
+
+    /*
+    var  person = new Person({
+        name: {
+            firstname: "Alvim",
+            lastname: "Brand"
+        },
+        age: 24
+    });
+
+    person.save(person, function(err, person){
+        if(err){
+            return;
+        }
+
+        res.send(person);
+    });
+    */
+
+
+    /*
+    Person.create({
+        name: {
+            firstname: 'Mateus',
+            lastname: 'Goncalves'
+        },
+        age: 23
+    }, function(err, person){
+        if(err){
+            return;
+        }
+        res.send(person);
+    });
+    */
+});
+
+router.put('/:id', function(req, res){
+    Person.findOneAndUpdate({
+        _id: req.params.id
+    }, {
+        name: {
+            firstname: 'Mateus 3',
+            lastname: 'Goncalves 3'
+        }
+    }, function(err, person){
+        if(err){
+            return;
+        }
+
+        res.send(person);
+    });
+
+    /*
+    Person.update({
+         _id: req.params.id
+    }, {
+        name: {
+            firstname: 'Mateus 1',
+            lastname: 'Goncalves 2'
+        }
+    }, function(err, person){
+        if(err){
+            return;
+        }
+
+        res.send(person);
+    });
+    */
+
+});
 module.exports = router;
